9e59e0f0a72802b8c23dcd4eba422e69b0099c56
ChiyunaZen <uni202406_11@telework.to>
2024-08-23 09:43:46 +0900

    SummonedCreatureクラスの追加

---
+++ SummonCreaturesApp/SummonedCreature.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SummonCreaturesApp
+{
+    public class SummonedCreature
+    {
+        //プロパティの設定
+        public string Name {  get; set; }
+        public int Level {  get; set; }
+
+        //コンストラクタの設定
+        public SummonedCreature(string name, int level)
+        {
+            Name = name;
+            Level =level;
+        }
+
+        //Attackメソッドの設定
+        public string Attack()
+        {
+            return $"{Name}がレベル{Level}で攻撃をした！";
+        }
+    }
+}
