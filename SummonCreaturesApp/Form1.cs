12f06be7c38590694b573fed5656ee7aa1c50076
ChiyunaZen <uni202406_11@telework.to>
2024-08-23 10:10:01 +0900

    攻撃機能の実装

--- SummonCreaturesApp/Form1.cs
+++ SummonCreaturesApp/Form1.cs
@@ -16,5 +16,22 @@ namespace SummonCreaturesApp
         {
             InitializeComponent();
         }
+
+        private void attackButton_Click(object sender, EventArgs e)
+        {
+            
+            if (creatureListBox.SelectedIndex>=0)
+            //creatureListBoxで一つ以上の要素が選択されている場合に実行される処理
+            {
+                SummonedCreature selectedCreature = summondCreature[creatureListBox.SelectedIndex];
+                //summondCreatureリスト内の選択している番号の要素を取り出し
+                actionResultLabel.Text = selectedCreature.Attack();
+                //取り出した要素のAttackメソッドを実行
+            }
+            else
+            {
+                actionResultLabel.Text = "攻撃するクリーチャーを選択してください";
+            }
+        }
     }
 }
