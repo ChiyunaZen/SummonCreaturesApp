a087dadcec91ad9292f17a297356cb7b495ed46b
TsubasaKato2024 <uni202406_06@telework.to>
2024-08-23 11:33:38 +0900

    攻撃button作成

--- SummonCreaturesApp_UserControl/Form1.cs
+++ SummonCreaturesApp_UserControl/Form1.cs
@@ -12,9 +12,23 @@ namespace SummonCreaturesApp_UserControl
 {
     public partial class Form1 : Form
     {
+        private List<SummonedCreature> summonedCreatures = new List<SummonedCreature>();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void attackButton_Click(object sender, EventArgs e)
+        {
+            SummonedCreature selectedCreature = summonControl.SelectedCreature;
+            if (selectedCreature != null)
+            {
+                actionResultLabel.Text = selectedCreature.Attack();
+            }
+            else
+            {
+                actionResultLabel.Text = "攻撃するクリーチャーを選択してください。";
+            }
         }
     }
 }
